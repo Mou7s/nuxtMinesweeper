2fe8d6ed4edb89a15a0774ae4c08d60140c6a3ee
mou7s <liuchuge7777777@gmail.com>
2023-08-28 23:12:46 +0800

    #

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,10 @@
+<script setup lang="ts"></script>
+
 <template>
-  <div>
+  <div class="text-9xl grid place-content-center min-h-screen">
     hello world!
   </div>
 </template>
+
+
+<style scoped></style>
