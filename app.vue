7c51ff2adc194d8788b166f7de4613f1e6edae4d
mou7s <liuchuge7777777@gmail.com>
2023-11-17 16:57:21 +0800

    #

--- app.vue
+++ app.vue
@@ -8,7 +8,7 @@ useSeoMeta({
 </script>
 <template>
   <main
-    class="py-10 text-center min-h-screen dark:bg-neutral-800 dark:text-neutral-200"
+    class="py-10 text-center min-h-screen max-h-screen dark:bg-neutral-800 dark:text-neutral-200"
   >
     <NuxtPage></NuxtPage>
     <Footer></Footer>
