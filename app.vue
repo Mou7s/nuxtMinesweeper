c089cedd672efc12df5dfaa929ddcf780ca9b87d
mou7s <liuchuge7777777@gmail.com>
2023-11-20 13:08:13 +0800

    #

--- app.vue
+++ app.vue
@@ -5,6 +5,16 @@ useSeoMeta({
   description: 'a minesweeper game powered by nuxt',
   ogDescription: 'a minesweeper game powered by nuxt',
 });
+
+useHead({
+  title: 'Minesweeper',
+  meta: [
+    {
+      name: 'google-site-verification',
+      content: 'uMb53AX2NHoWvhBjQGUPiU2EvzBs2pWg90HYPMkcMP4',
+    },
+  ],
+});
 </script>
 <template>
   <NuxtLoadingIndicator></NuxtLoadingIndicator>
