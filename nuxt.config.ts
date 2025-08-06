1e94e9ab5997dae0e62c4e4129e0b1252d924083
mou7s <liuchuge7777777@gmail.com>
2025-08-06 17:22:10 +0800

    Revert "Update nuxt.config.ts and package.json"
    This reverts commit 11af132c0c4eef0e2ea3c02be4bb2ec7913d3b35.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,9 +2,4 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ['@vueuse/nuxt', '@nuxt/ui'],
-  compatibilityDate: '2025-02-14',
-
-  devServer: {
-    port: 80,
-  },
 });
